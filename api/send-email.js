b63c4cda4d73d11b359ba039b2545b01ab105d04
Heming Liu <hemingliu123@gmail.com>
2024-03-23 18:43:39 -0400

    update

---
+++ api/send-email.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+module.exports = (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Configure nodemailer
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'hemingliu123@gmail.com',
+      pass: 'hqcg msvm ojis onhh' // gmail App Password
+    }
+  });
+
+  // Configure the email options
+  const mailOptions = {
+    from: 'hemingliu123@gmail.com',
+    to: 'hemingliu123@gmail.com', // Recipient email address
+    subject: 'New Message from Your Website',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Something went wrong');
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+};
